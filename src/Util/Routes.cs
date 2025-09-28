1f27276111b8bd00351cf5a8bf09eb421867d55b
godchisa254 <Matias.254@outlook.com>
2025-09-28 01:06:33 -0300

    chore(api): add route constants for API endpoint definitions

--- src/Util/Routes.cs
+++ src/Util/Routes.cs
@@ -6,6 +6,8 @@ public class Routes
     public const string UserRoute = "http://localhost:2020/api/users";
 
     public const string RoutesRoute = "http://localhost:4000/api/routes";
+
+    public const string StationsRoute = "http://localhost:9090/api/stations";
     
     private Routes() {}
     
