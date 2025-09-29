a7bf5da3d86fd56ce71b474c644ef4f9501298f6
grenzya <ignacio.bugueno02@alumnos.ucn.cl>
2025-09-28 23:42:19 -0300

    refactor: rename route properties to use 'Id' suffix for clarity

--- src/Dto/CreationRouteRequest.cs
+++ src/Dto/CreationRouteRequest.cs
@@ -8,16 +8,16 @@ namespace perla_metro_main_api.Dto
     {
         [Required(ErrorMessage = "El origen es obligatorio.")]
         [StringLength(200, ErrorMessage = "El origen no puede exceder los 200 caracteres.")]
-        public string origin { get; set; }
+        public string originId { get; set; }
 
         [Required(ErrorMessage = "El destino es obligatorio.")]
         [StringLength(200, ErrorMessage = "El destino no puede exceder los 200 caracteres.")]
-        public string destination { get; set; }
+        public string destinationId { get; set; }
 
         /// <summary>
         /// Paradas intermedias
         /// </summary>
-        public List<string> stops { get; set; } = new();
+        public List<string> stopsIds { get; set; } = new();
 
         [Required(ErrorMessage = "La hora de inicio es obligatoria.")]
         [RegularExpression(@"^(?:[01]\d|2[0-3]):[0-5]\d$", 
